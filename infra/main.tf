5bf8c9c744f7d6f1a5eeb1047f32652e357eea85
Umar Atimomo <umar.atimomo@gmail.com>
2026-07-09 00:18:48 +0300

    fix: restore missing security groups and update ecs module outputs

--- infra/main.tf
+++ infra/main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "ecs_tasks" {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
+    security_groups = [module.alb.id]
   }
 
   # Outbound traffic allowed anywhere (to talk to VPC Endpoints / ECR)
