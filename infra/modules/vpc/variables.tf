169e74211a3b72453034689034e954271e9d96e7
Umar Atimomo <umar.atimomo@gmail.com>
2026-07-08 17:52:12 +0300

    fix: add missing variable declarations to vpc module

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,3 +1,12 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "project_name" {
+  description = "The name of the project to be used in tags"
+  type        = string
+}
 # locals {
 #   subnets = {
 #     "public-a" = { cidr = "10.0.1.0/24", az = "eu-north-1a", public = true }
