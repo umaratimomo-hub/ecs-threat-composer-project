c3ac8829abef60b36fcb8f6519f06c63a3e1a06a
Umar Atimomo <umar.atimomo@gmail.com>
2026-07-10 11:19:07 +0300

    feat: unify project name variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,5 @@
 variable "project_name" {
   description = "The name of the project"
   type        = string
-  Default     = "ecs-threat-composer-project" #
+  default     = "ecs-threat-composer-project" #
 }
