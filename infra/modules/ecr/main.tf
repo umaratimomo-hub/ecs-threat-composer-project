12871f5202a28fbc833f0494cb21bf2d085c0fe7
Umar Atimomo <umar.atimomo@gmail.com>
2026-07-13 18:52:43 +0300

    unify container naming in ecs main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -8,6 +8,9 @@ resource "aws_ecr_repository" "app_repo" {
 
 # Push a placeholder image immediately
 resource "null_resource" "push_placeholder_image" {
+    triggers = {
+    repository_url = aws_ecr_repository.app_repo.repository_url
+    
   # Script runs on local machine during 'terraform apply'
   provisioner "local-exec" {
     # Uses && to chain commands together to avoid windows line ending problems
