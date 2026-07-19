16ca5d0ae3206c5a53d94df1c3770f2224a11260
Umar Atimomo <umar.atimomo@gmail.com>
2026-07-19 18:32:01 +0300

    clean up alb main

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ module "alb" {
   project_name      = var.project_name
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
-  certificate_arn    = module.acm.acm_certificate_arn
+  certificate_arn    = module.acm.certificate_arn
   cloudflare_zone_id = var.cloudflare_zone_id
 }
 
